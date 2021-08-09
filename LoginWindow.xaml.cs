bd52a937e8e5266a32a984a116b1115887b9f187
Anastasia <nastya05538@yandex.ru>
2021-08-09 20:44:19 +0300

    Добавьте файлы проекта.

---
+++ LoginWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace CRM_Shop
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            CRMContext context = new CRMContext();
+
+            var user = context.GetUserData(Login_TB.Text, Password_PB.Password);
+
+            foreach (var u in user)
+            {
+                CRMUser User = new CRMUser(u.Login, u.RoleName, u.EmployeeName);
+            }
+        }
+    }
+}
